33e53716b61022728a8e0fe78580cbc8733f0db4
chondominguez <chondominguez55@gmail.com>
2020-10-30 19:59:01 -0300

    Producto

--- DATOS BASE.sql
+++ DATOS BASE.sql
@@ -7,6 +7,15 @@ dni int unique,
 apellido varchar(30) not null,
 nombre varchar(30) not null,
 puesto varchar(30) not null,
+id_tarjeta int unique,
+user varchar(30) not null unique,
+pass varchar(30) not null,
 legajo int unique auto_increment,
 PRIMARY KEY (legajo)
+);
+
+CREATE TABLE Producto(
+codigo int unique,
+nom_prod varchar(50) not null
+PRIMARY KEY (codigo)
 );
