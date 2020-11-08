a9d91ca141a2f93b51963c4b4f2caef8568e4874
chondominguez <chondominguez55@gmail.com>
2020-11-08 12:15:18 -0300

    unificacion de script

--- DATOS BASE.sql
+++ DATOS BASE.sql
@@ -1,8 +1,8 @@
-CREATE DATABASE CreaTecAlgabo;
+create database CreaTecAlgabo;
 
-USE CreaTecAlgabo;
+use CreaTecAlgabo;
 
-CREATE TABLE Empleado(
+create table Empleado(
 dni int unique,
 apellido varchar(30) not null,
 nombre varchar(30) not null,
@@ -11,11 +11,42 @@ id_tarjeta int unique,
 user varchar(30) not null unique,
 pass varchar(30) not null,
 legajo int unique auto_increment,
-PRIMARY KEY (legajo)
+primary key (legajo)
 );
 
-CREATE TABLE Producto(
+create table Producto(
 codigo int unique,
 nom_prod varchar(50) not null
-PRIMARY KEY (codigo)
+primary key (codigo)
 );
+
+create table lecturasRFID(
+uid int ,
+idlector int unique,
+fecha date,
+hora time not null,
+primary key(idlector)
+);
+
+insert into lecturasRFID values
+('1','1','1990-10-08','8:30'),
+('2','2','2000-06-04','9:45');
+
+select * from lecturasRFID;
+
+
+create table accesoPersonal(
+uid int ,
+idmolinete int unique,
+sentido varchar(30) not null,
+fecha date,
+hora time not null,
+primary key(idmolinete)
+);
+
+
+insert into accesoPersonal values
+('1','1','entrada','1990-10-08','10:30'),
+('2','2','salida','2000-06-04','12:45');
+
+select * from accesoPersonal;
