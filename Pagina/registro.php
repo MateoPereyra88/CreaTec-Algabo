c34c1856a72e7bc27095befe24a787a3f0c63e10
chondominguez <chondominguez55@gmail.com>
2020-11-25 00:31:47 -0300

    Creacion de paginas

--- Pagina/registro.php
+++ Pagina/registro.php
@@ -8,9 +8,7 @@
 	if ($conexion===false) {
 		die("error de conexion".mysqli_connect_error());
 	}
-	$sql = "select * from Empleado where ((IdTarjeta = '".$_POST['id_tarjeta']."')";
-	$sqli1 = "insert into lecturasRFID values (IdTarjeta,IdLector,Curdate(fecha),Curtime(hora))";
-	$sqli2 = "insert into accesoPersonal values (IdTarjeta,IdMolinete,Curdate(fecha),Curtime(hora))";
+	$sql = "insert into Empleado ";
 	$resultado = mysqli_query($conexion, $sql);
 	if(mysqli_num_rows($resultado) == 1){
 		mysqli_query($sqli1);
